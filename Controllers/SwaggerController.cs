dce5fadaf15a4c584233e23d345c17999b796727
kid <kid-jc@hotmail.com>
2023-06-23 02:30:58 +0800

    建立示範 Swagger 文件匯出工具
    - 加入 SwaggerDoc.cshtml 範本
    - 加入匯出時所需的工具方法
      - ByteHelper.cs
      - HtmlHelper.cs
      - SpireDocHelper.cs
    - 加入匯出設定檔案
      - docTemp.xml

---
+++ Controllers/SwaggerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Swagger2Docx.Helper;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Swagger2Docx.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class SwaggerController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        private readonly SwaggerGenerator _swaggerGenerator;
+
+        private readonly SpireDocHelper _spireDocHelper;
+
+        public SwaggerController(IWebHostEnvironment hostingEnvironment, SpireDocHelper spireDocHelper, SwaggerGenerator swaggerGenerator)
+        {
+            _webHostEnvironment = hostingEnvironment;
+            _spireDocHelper = spireDocHelper;
+            _swaggerGenerator = swaggerGenerator;
+        }
+        /// <summary>
+        /// 匯出文件
+        /// </summary>
+        /// <param name="type">檔案類型</param>
+        /// <param name="version">版本號V1</param>
+        /// <returns></returns>
+        [HttpGet]
+        public FileResult ExportWord(string type, string version)
+        {
+            string contenttype = string.Empty;
+
+            var model = _swaggerGenerator.GetSwagger(version); //1. 根據指定版本獲取指定版本的json物件。
+
+            var html = HtmlHelper.GeneritorSwaggerHtml($"{_webHostEnvironment.WebRootPath}\\SwaggerDoc.cshtml", model); //2. 根據範本引擎生成html
+
+            var op = _spireDocHelper.SwaggerConversHtml(html, type, out contenttype); //3.將html匯出檔案類型
+
+            return File(op, contenttype, $"XUnit.Core介面文檔{type}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Swagger2Docx
 Swagger to Docx Tool
+
+
+# 參考來源
+
+- [.Net core|C#：swagger导出文档](https://juejin.cn/post/7033723875020144647)
