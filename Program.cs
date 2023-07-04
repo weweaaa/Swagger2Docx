c7f985e8dc309418d55504ee0021a5f60111c8ab
jia <jia@demo.com>
2023-07-04 16:38:07 +0800

    建立基礎 MinimalAPI 專案

---
+++ Program.cs
@@ -0,0 +1,54 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(x => x.FullName);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/Weatherforecast", () =>
+{
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Swagger2Docx
+Swagger to Docx Tool
+
+## 使用方式
+
+[開發網址 - http://localhost:xxxx/swagger/index.html](http://localhost:xxxx/swagger/index.html)
+
+![匯出示範](https://github.com/weweaaa/Swagger2Docx/assets/20127862/f5da38e3-c23a-4697-868c-f4f28600d97c)
+
+## 參考來源
+
+- [.Net core|C#：swagger导出文档](https://juejin.cn/post/7033723875020144647)
